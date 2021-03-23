2464ea061a18e67491032892c1ff82ea93054e7c
Vitech-UA <viktorstarovit@gmail.com>
2021-03-23 21:03:58 +0200

    Додав приклад роботи з кортежами

--- Python_tuple/Python_tuple.py
+++ Python_tuple/Python_tuple.py
@@ -2,4 +2,16 @@ empty_tuple = ()
 print(empty_tuple)
 
 
-mark_tuple = 'Groucho','Chico','Harpo'
+mark_tuple = ('Groucho','Chico','Harpo')
+print(mark_tuple)
+
+a,b,c = mark_tuple #Розпаковка кортежа, кожній змінній присвоюється елемент кортежу
+print(a)
+print(b)
+print(c)
+
+
+mark_list = ['One', 'Two', 'Tree']
+print(mark_list) #Виведеться, як список
+tuper_from_list = tuple(mark_list) #Перетворюємо у кортеж дані передані у функцію tuple
+print(tuper_from_list) #Виведеться, як кортеж
