ed61b61205e3430858cb4b97faa3e6e5f3724873
DESKTOP-C4LU9U7\Vitech-UA <viktorstarovit@gmail.com>
2021-07-22 22:50:31 +0300

    upd

--- Kormen_insertion_sort/main.py
+++ Kormen_insertion_sort/main.py
@@ -1,12 +1,14 @@
-input_data = [23, 43, 22, 1, 2, 7, 8, 0, 98, 1999]
+input_data = [23, 43, 22, 1, 2, 22, 7, 8, 0, 98, 1999]
 
-for j in range(2, len(input_data)):
-    key = input_data[j]
+for j in range(1, len(input_data) - 1):
     i = j - 1
+    key = input_data[j]
+
     while i >= 0 and input_data[i] > key:
         input_data[i + 1] = input_data[i]
         i = i - 1
-    input_data[i + 1] = key
-print(input_data)
+    input_data[i+1] = key
 
+
+print(input_data)
 
