aacd9a7a77ed84aeac3a94eb44e9bb43edcb629a
DESKTOP-C4LU9U7\Vitech-UA <viktorstarovit@gmail.com>
2021-07-29 20:41:03 +0300

    Переписав простіше алгоритм сортування вставкою. Кормен. Глава 2.

--- Kormen_insertion_sort/main.py
+++ Kormen_insertion_sort/main.py
@@ -1,14 +1,11 @@
-input_data = [23, 43, 22, 1, 2, 22, 7, 8, 0, 98, 1999]
+input_data_ = [12, 0, 3, 43, 22, 1, 2, 23, 7, 8, 0, 98, 1999]
 
-for j in range(1, len(input_data) - 1):
-    i = j - 1
-    key = input_data[j]
-
-    while i >= 0 and input_data[i] > key:
-        input_data[i + 1] = input_data[i]
-        i = i - 1
-    input_data[i+1] = key
-
-
-print(input_data)
-
+print("До сортування:   ", input_data_)
+for i in range(len(input_data_)):
+    k = i
+    while k > 0 and input_data_[k - 1] > input_data_[k]:
+        tmp = input_data_[k - 1]
+        input_data_[k - 1] = input_data_[k]
+        input_data_[k] = tmp
+        k -= 1
+print("Після сортування:", input_data_)
